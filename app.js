7bf4edb94b57aaf64554aa20d44a44bbd8eb9593
Fabrizio <Fabrizio_39@hotmail.com>
2019-08-11 11:02:25 +0200

    remove static css/js files and map from node modules

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.use(morgan('tiny'));
 
 // bring in static files
 app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
 // ...
 app.get('/', (req, res) => {
