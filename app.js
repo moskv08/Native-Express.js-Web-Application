8dd3c93d298cd6373ca35cf6388fdb2a91ad9ebc
Fabrizio <Fabrizio_39@hotmail.com>
2019-08-11 10:55:32 +0200

    use morgan for traffic metric and bring in static files

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
+// Log web traffic to console
+app.use(morgan('tiny'));
+
+// bring in static files
+app.use(express.static(path.join(__dirname, '/public/')));
+
+// ...
 app.get('/', (req, res) => {
-    res.send('Hello World.');
+    res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
 app.listen(3000, () => {
