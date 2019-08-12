3434df3e975ebf45c4f8cae189b2c7a49da92a20
Fabrizio <Fabrizio_39@hotmail.com>
2019-08-12 21:13:48 +0200

    Bring in ejs template engine

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Bring in modules
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
@@ -6,6 +7,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 // Log web traffic to console
 app.use(morgan('tiny'));
 
@@ -15,7 +17,11 @@ app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
-// ...
+// Set views directory & template engine
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+// Define get routes
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'));
 });
