c42681e7d69ac03a87a6fd3dc93023e8587078e6
Fabrizio <Fabrizio_39@hotmail.com>
2019-08-14 21:27:08 +0200

    wrapped bookroute into an exported function

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,48 +1,52 @@
 // Bring in modules
 const express = require('express');
 
-const books = [
-  {
-    title: 'The holly grail',
-    genre: 'Fantasy',
-    author: 'John Doe',
-    read: false,
-  },
-  {
-    title: 'Life after',
-    genre: 'Romance',
-    author: 'Jennifer Rowl',
-    read: false,
-  },
-  {
-    title: 'Peter Pan',
-    genre: 'Fantasy',
-    author: 'Marry Stripes',
-    read: false,
-  },
-];
-
 const bookRouter = express.Router();
 
-// Define get routes
-bookRouter.route('/')
-  .get((req, res) => {
-    res.render('books',
+function router(nav) {
+  const books = [
+    {
+      title: 'The holly grail',
+      genre: 'Fantasy',
+      author: 'John Doe',
+      read: false,
+    },
+    {
+      title: 'Life after',
+      genre: 'Romance',
+      author: 'Jennifer Rowl',
+      read: false,
+    },
+    {
+      title: 'Peter Pan',
+      genre: 'Fantasy',
+      author: 'Marry Stripes',
+      read: false,
+    },
+  ];
+
+  // Define get routes
+  bookRouter.route('/')
+    .get((req, res) => {
+      res.render('bookListView',
+        {
+          title: 'List of Books',
+          nav,
+          books,
+        });
+    });
+
+  bookRouter.route('/:id').get((req, res) => {
+    const { id } = req.params;
+    res.render('bookView',
       {
-        title: 'Books',
-        nav: [{ title: 'Books', link: '/books' }],
-        books,
+        title: 'Book',
+        nav,
+        book: books[id],
       });
   });
 
-bookRouter.route('/:1').get((req, res) => {
-  const { id } = req.params;
-  res.render('book',
-    {
-      title: 'Books',
-      nav: [{ title: 'Books', link: '/books' }],
-      book: books[id],
-    });
-});
+  return bookRouter;
+}
 
-module.exports = bookRouter;
+module.exports = router;
