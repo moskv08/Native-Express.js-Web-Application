8ece9d45abe28c11c45fa6cb179761750151f705
Fabrizio <Fabrizio_39@hotmail.com>
2019-08-16 11:28:30 +0200

    finalize db schema

--- sql_scripts/dbschema.sql
+++ sql_scripts/dbschema.sql
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS genres(
    	genre_id serial PRIMARY KEY, -- implicit primary key constraint
    	genre VARCHAR (50) NOT NULL
 );
-DROP TABLE authors
+
 -- Authors table
 CREATE TABLE IF NOT EXISTS authors(
    	author_id int NOT NULL UNIQUE PRIMARY KEY, -- implicit primary key constraint
@@ -51,17 +51,14 @@ INSERT INTO authors (author_id, surname, givenname) VALUES
 	
 -- Insert Books
 INSERT INTO books (isbn, title, genre_id, author_id) VALUES
-    ('888123420', 'Hope in the Wind', (SELECT genre_id from genre WHERE genre='Romance'), 1),
-	('223235232', 'The art of war', (SELECT genre_id from genre WHERE genre='Biography'), 1),
-	('200123948', 'Lord of the rings', (SELECT genre_id from genre WHERE genre='Fiction'), 1),
-	('900238142', 'Duty of Love', (SELECT genre_id from genre WHERE genre='Romance'), 3),
-	('391798736', 'Backyard Thugs', (SELECT genre_id from genre WHERE genre='Crime'), 2),
-	('599987333', 'Piggy Bank robbery', (SELECT genre_id from genre WHERE genre='Crime'), 2),
-	('230097415', 'Moo the cat', (SELECT genre_id from genre WHERE genre='Kids'), 3);
+    ('888123420', 'Hope in the Wind', (SELECT genre_id from genres WHERE genre='Romance'), 1),
+	('223235232', 'The art of war', (SELECT genre_id from genres WHERE genre='Biography'), 1),
+	('200123948', 'Lord of the rings', (SELECT genre_id from genres WHERE genre='Fiction'), 1),
+	('900238142', 'Duty of Love', (SELECT genre_id from genres WHERE genre='Romance'), 3),
+	('391798736', 'Backyard Thugs', (SELECT genre_id from genres WHERE genre='Crime'), 2),
+	('599987333', 'Piggy Bank robbery', (SELECT genre_id from genres WHERE genre='Crime'), 2),
+	('230097415', 'Moo the cat', (SELECT genre_id from genres WHERE genre='Kids'), 3);
 
-	
-	   
-	   
 	   
 	   
 	   
