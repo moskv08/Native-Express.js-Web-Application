5eb7a4f55c621202cf95132e19f9307ad1b9a5cc
Fabrizio <Fabrizio_39@hotmail.com>
2019-08-23 10:20:16 +0200

    migrate genre into enum

--- sql_scripts/dbschema.sql
+++ sql_scripts/dbschema.sql
@@ -1,6 +1,6 @@
 -- Database: ExpressDB
 
-DROP DATABASE "ExpressDB";
+-- DROP DATABASE "ExpressDB";
 CREATE DATABASE "ExpressDB"
     WITH 
     OWNER = postgres
@@ -10,38 +10,26 @@ CREATE DATABASE "ExpressDB"
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
--- Genres table
-CREATE TABLE IF NOT EXISTS genres(
-   	genre_id serial PRIMARY KEY, -- implicit primary key constraint
-   	genre VARCHAR (50) NOT NULL
-);
-
 -- Authors table
 CREATE TABLE IF NOT EXISTS authors(
-   	author_id int NOT NULL UNIQUE PRIMARY KEY, -- implicit primary key constraint
+   	author_id INT NOT NULL UNIQUE PRIMARY KEY, -- implicit primary key constraint
    	surname VARCHAR (50) NOT NULL,
    	givenname VARCHAR (20) NOT NULL
 );
 
 -- Books table
+CREATE TYPE genres AS ENUM ('Crime','Romance','Fiction','Adventure','Kids','Biography','Undefined');
 CREATE TABLE IF NOT EXISTS books(
-   	isbn int NOT NULL UNIQUE PRIMARY KEY, -- implicit primary key constraint
+   	isbn INT NOT NULL UNIQUE PRIMARY KEY, -- implicit primary key constraint
    	title VARCHAR (50) NOT NULL,
-	genre_id integer not null references genres(genre_id),
-	author_id integer not null references authors(author_id)
+	genre genres NOT NULL DEFAULT 'Undefined'
+	author_id INTEGER NOT NULL REFERENCES authors(author_id)
 );
-
--- Mapping table for books and authors
--- CREATE TABLE books_authors (
--- 	book_id    	int REFERENCES books (isbn) ON UPDATE CASCADE ON DELETE CASCADE,
--- 	author_id 	int REFERENCES authors (author_id) ON UPDATE CASCADE,
--- 	CONSTRAINT book_author_pkey PRIMARY KEY (book_id, author_id)  -- explicit pk
--- );
 
 /* -------------------------------------------------------------------------------------- */
 -- Insert Genres
-INSERT INTO genres (genre) VALUES
-    ('Crime'),('Romance'),('Fiction'),('Adventure'),('Kids'),('Biography');
+-- INSERT INTO genres (genre) VALUES
+    -- ('Crime'),('Romance'),('Fiction'),('Adventure'),('Kids'),('Biography');
 
 -- Insert Authors
 INSERT INTO authors (author_id, surname, givenname) VALUES
@@ -50,14 +38,14 @@ INSERT INTO authors (author_id, surname, givenname) VALUES
 	('3', 'Hitchcock', 'Alfred');
 	
 -- Insert Books
-INSERT INTO books (isbn, title, genre_id, author_id) VALUES
-    ('888123420', 'Hope in the Wind', (SELECT genre_id from genres WHERE genre='Romance'), 1),
-	('223235232', 'The art of war', (SELECT genre_id from genres WHERE genre='Biography'), 1),
-	('200123948', 'Lord of the rings', (SELECT genre_id from genres WHERE genre='Fiction'), 1),
-	('900238142', 'Duty of Love', (SELECT genre_id from genres WHERE genre='Romance'), 3),
-	('391798736', 'Backyard Thugs', (SELECT genre_id from genres WHERE genre='Crime'), 2),
-	('599987333', 'Piggy Bank robbery', (SELECT genre_id from genres WHERE genre='Crime'), 2),
-	('230097415', 'Moo the cat', (SELECT genre_id from genres WHERE genre='Kids'), 3);
+INSERT INTO books (isbn, title, genre, author_id) VALUES
+    ('888123420', 'Hope in the Wind', 'Romance', 1),
+	('223235232', 'The art of war', 'Biography', 1),
+	('200123948', 'Lord of the rings', 'Fiction', 1),
+	('900238142', 'Duty of Love', 'Romance', 3),
+	('391798736', 'Backyard Thugs', 'Crime', 2),
+	('599987333', 'Piggy Bank robbery', 'Crime', 2),
+	('230097415', 'Moo the cat', 'Kids', 3);
 
 	   
 	   
