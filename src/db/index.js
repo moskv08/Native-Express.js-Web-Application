4cb76ba648b461d9d4f60ea0dce7bf249dfa1722
Fabrizio <Fabrizio_39@hotmail.com>
2019-08-23 09:57:36 +0200

    Implemented pool connection in repositroy pattern

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,10 @@
 // Import
 const { Pool } = require('pg');
 
+// Create connection pool
 const pool = new Pool();
+
+// Export
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
