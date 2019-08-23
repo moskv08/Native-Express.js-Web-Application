8c4f837371a870aa3f5473f3ccd045725e1738ef
Fabrizio <Fabrizio_39@hotmail.com>
2019-08-23 15:50:27 +0200

    using .all and keep the rendring for .get

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -8,34 +8,41 @@ const bookRouter = express.Router();
 function router(nav) {
   // Get routes for a list of books
   bookRouter.route('/')
-    .get((req, res) => {
+    .all((req, res, next) => {
       (async function query() {
         // Querry
-        const { rows } = await db.query('SELECT * FROM books');
-        res.render('bookListView',
-          {
-            title: 'List of Books',
-            nav,
-            books: rows,
-          });
+        const { rows } = await db.query('SELECT isbn, title, genre FROM books');
+        [req.books] = rows;
+        next();
       }());
+    }).get((req, res) => {
+      res.render('bookListView',
+        {
+          title: 'List of Books',
+          nav,
+          books: req.books,
+        });
     });
 
   // Get routes for a single book
-  bookRouter.route('/:id').get((req, res) => {
-    (async function query() {
-      const { id } = req.params;
-
-      // Querry
-      const { rows } = await db.query('SELECT * FROM books WHERE isbn = $1', [id]);
+  bookRouter.route('/:id')
+    .all((req, res, next) => {
+      (async function query() {
+        const { id } = req.params;
+        // Querry
+        const { rows } = await db.query('SELECT * FROM books b INNER JOIN authors a ON (b.author_id = a.author_id) WHERE isbn = $1', [id]);
+        [req.book] = rows; // Use array destructuring
+        next();
+      }());
+    })
+    .get((req, res) => {
       res.render('bookView',
         {
           title: 'Book',
           nav,
-          book: rows,
+          book: req.book,
         });
-    }());
-  });
+    });
 
   return bookRouter;
 }
