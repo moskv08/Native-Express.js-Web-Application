70b305b0f671c5d160b4fd6781c85232dcb649c7
Fabrizio <Fabrizio_39@hotmail.com>
2019-08-29 21:38:32 +0200

    create login routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -9,8 +9,17 @@ function router() {
   authRouter.route('/signUp')
     .post((req, res) => {
       debug(req.body);
-      res.json(req.body);
+      // Placeholder for: Create the user
+      req.login(req.body, () => {
+        res.redirect('/auth/profile');
+      });
     });
+
+  authRouter.route('/profile')
+    .get((req, res) => {
+      res.json(req.user);
+    });
+
   return authRouter;
 }
 
