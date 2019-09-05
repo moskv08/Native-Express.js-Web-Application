a95d3a11f3206eec412bbf396c415606e86a0bec
Fabrizio <Fabrizio_39@hotmail.com>
2019-09-05 21:41:53 +0200

    connect local auth strategy to the database

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -1,6 +1,9 @@
-// deals with user information in sessions/cookies
+const debug = require('debug')('app:local.strategy');
+// Bring in modules that deals with user information in sessions/cookies
 const passport = require('passport');
 const { Strategy } = require('passport-local');
+// Bring in dbcontext
+const db = require('../../db');
 
 // local strategy
 function localStrategy() {
@@ -8,10 +11,22 @@ function localStrategy() {
     usernameField: 'email',
     passwordField: 'password',
   }, (email, password, done) => {
-    const user = {
-      email, password,
-    };
-    done(null, user);
+    (async function verifyUser() {
+      const query = 'SELECT * FROM users WHERE email = $1';
+      const values = [email];
+
+      try {
+        // Try to get the user
+        const { rows } = await db.query(query, values);
+        if (rows[0].password === password) {
+          done(null, rows);
+        } else {
+          done(null, false);
+        }
+      } catch (error) {
+        debug(error.stack);
+      }
+    }());
   }));
 }
 
