1862d4b6bef5c3d564d2c3acbee2003425c7b04f
Fabrizio <Fabrizio_39@hotmail.com>
2019-09-05 21:56:14 +0200

    Protect book routes

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -9,6 +9,15 @@ const db = require('../db');
 const bookRouter = express.Router();
 
 function router(nav) {
+  // Protect the route
+  bookRouter.use((req, res, next) => {
+    if (req.user) {
+      next();
+    } else {
+      res.redirect('/');
+    }
+  });
+
   // Get routes for a list of books
   bookRouter.route('/')
     .all((req, res, next) => {
