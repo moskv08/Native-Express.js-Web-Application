e5ae34cfbffa58add0a4bfd7e625031f9ff80298
Fabrizio <Fabrizio_39@hotmail.com>
2019-09-08 20:32:51 +0200

    move get logic for books to the controller

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,14 +1,14 @@
 // Bring in modules
 const express = require('express');
-// const chalk = require('chalk');
-// const debug = require('debug')('app:bookRoutes');
-
+const bookController = require('../controllers/bookController');
 // Bring in dbcontext
 const db = require('../db');
 
 const bookRouter = express.Router();
 
 function router(nav) {
+
+  const { getAllBooks, getBookById } = bookController(nav);
   // Protect the route
   bookRouter.use((req, res, next) => {
     if (req.user) {
@@ -19,42 +19,10 @@ function router(nav) {
   });
 
   // Get routes for a list of books
-  bookRouter.route('/')
-    .all((req, res, next) => {
-      (async function query() {
-        // Querry
-        const { rows } = await db.query('SELECT isbn, title, genre FROM books');
-        req.books = rows;
-        next();
-      }());
-    }).get((req, res) => {
-      res.render('books/bookListView',
-        {
-          title: 'List of Books',
-          nav,
-          books: req.books,
-        });
-    });
+  bookRouter.route('/').get(getAllBooks);
 
   // Get routes for a single book
-  bookRouter.route('/:id')
-    .all((req, res, next) => {
-      (async function query() {
-        const { id } = req.params;
-        // Querry
-        const { rows } = await db.query('SELECT * FROM books b INNER JOIN authors a ON (b.author_id = a.author_id) WHERE isbn = $1', [id]);
-        [req.book] = rows; // Use array destructuring
-        next();
-      }());
-    })
-    .get((req, res) => {
-      res.render('books/bookItemView',
-        {
-          title: 'Book',
-          nav,
-          book: req.book,
-        });
-    });
+  bookRouter.route('/:id').get(getBookById);
 
   return bookRouter;
 }
