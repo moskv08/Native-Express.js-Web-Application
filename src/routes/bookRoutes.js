0e3f9fe60d995752c2eaa844d426fb045c3796a6
Fabrizio <Fabrizio_39@hotmail.com>
2019-09-11 19:29:45 +0200

    inject bookService

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,13 +1,14 @@
 // Bring in modules
 const express = require('express');
 const bookController = require('../controllers/bookController');
+const bookService = require('../services/bookService'):
 
 const bookRouter = express.Router();
 
 function router(nav) {
 
   // Bring functions as objects
-  const { getAllBooks, getBookById, useMiddleware } = bookController(nav);
+  const { getAllBooks, getBookById, useMiddleware } = bookController(bookService, nav);
 
   // Protect the route
   bookRouter.use(useMiddleware);
