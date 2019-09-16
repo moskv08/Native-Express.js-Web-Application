7ab671daecc355cc5932fd8eacf475a8e80f79c3
Fabrizio <Fabrizio_39@hotmail.com>
2019-09-16 21:09:42 +0200

    initial a first unit test

---
+++ test/controllers/bookController.spec.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const bookContorller = require('../../src/controllers/bookController');
+
+describe('bookController', () => {
+  describe('useMiddleware()', () => {
+    it('should authenticate a user', () => {
+      assert.equal(2, 2);
+    });
+  });
+});
