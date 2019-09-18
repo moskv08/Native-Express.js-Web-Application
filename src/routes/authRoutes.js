c3a8a5f2804e2500cb25b10f47aad9cd2cf02ffb
Robb Moskv <fabrizio_39@hotmail.com>
2019-09-18 19:17:39 +0200

    exlcude auth logic into controller

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,65 +1,35 @@
-const debug = require('debug')('app:authRoutes');
+// const debug = require('debug')('app:authRoutes');
 // Bring in modules
 const express = require('express');
 const passport = require('passport');
-// Bring in dbcontext
-const db = require('../db');
+const authController = require('../controllers/authController');
 
 const authRouter = express.Router();
 
 function router(nav) {
+  // Bring in functions as objects
+  const {
+    signUpUser,
+    showUserProfil,
+    showLoginPage,
+  } = authController(nav);
+
   // Create & sign in a user
-  authRouter.route('/signUp')
-    .post((req, res) => {
-      // Create the user
-      const { email, password } = req.body;
-      (async function addUser() {
-        const query = 'INSERT INTO users (email, password) VALUES ($1, $2) RETURNING userid';
-        const values = [email, password];
+  authRouter.route('/signUp').post(signUpUser);
 
-        try {
-          await db.query('BEGIN');
-          const { rows } = await db.query(query, values);
-          // [req.users] = rows;
-          await db.query('COMMIT');
-          // Redirect
-          req.login(rows[0], () => {
-            res.redirect('/auth/profile');
-          });
-
-          debug(rows);
-        } catch (error) {
-          await db.query('ROLLBACK');
-          throw error;
-        }
-      }()).catch((e) => debug(e.stack));
-    });
   // Sign in a user
   authRouter.route('/signin')
-    .get((req, res) => {
-      res.render('signin', {
-        nav,
-        title: 'Sign In',
-      });
-    })
-    .post(passport.authenticate('local', {
-      successRedirect: '/auth/profile',
-      failureRedirect: '/auth/signin',
-      failureFlash: true,
-    }));
+    .get(showLoginPage)
+    .post(
+      passport.authenticate('local', {
+        successRedirect: '/auth/profile',
+        failureRedirect: '/auth/signin',
+        failureFlash: true,
+      }),
+    );
+
   // Display the user profile
-  authRouter.route('/profile')
-    // Protect route (user validation)
-    .all((req, res, next) => {
-      if (req.user) {
-        next();
-      } else {
-        res.redirect('/');
-      }
-    })
-    .get((req, res) => {
-      res.json(req.user);
-    });
+  authRouter.route('/profile').get(showUserProfil);
 
   return authRouter;
 }
