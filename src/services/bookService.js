0606b3764c7a462f8fb4df1ecbf626900cfc5ba6
Fabrizio <Fabrizio_39@hotmail.com>
2019-10-01 21:29:06 +0200

    handle reject

--- src/services/bookService.js
+++ src/services/bookService.js
@@ -6,16 +6,28 @@ function bookService() {
   // Get all books from database
   function getAllBooks() {
     return new Promise((resolve, reject) => {
+      // Query database
       const result = db.query('SELECT isbn, title, genre FROM books');
-      resolve(result);
+      if (result) {
+        resolve(result);
+      } else {
+        const err = new Error('Sorry. Something went wrong here.');
+        reject(err);
+      }
     });
   }
 
   // Get book by an Id
   function getBookById(id) {
     return new Promise((resolve, reject) => {
+      // Query database
       const result = db.query('SELECT * FROM books b INNER JOIN authors a ON (b.author_id = a.author_id) WHERE isbn = $1', [id]);
-      resolve(result);
+      if (result) {
+        resolve(result);
+      } else {
+        const err = new Error('Sorry. Something went wrong here.');
+        reject(err);
+      }
     });
   }
 
