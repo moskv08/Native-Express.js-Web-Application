9e2e6ba73b93b3a84353a2cf817cd69bfad164c4
Fabrizio <Fabrizio_39@hotmail.com>
2019-10-01 21:22:09 +0200

    add a test for the bookService

---
+++ test/services/bookService.spec.js
@@ -0,0 +1,22 @@
+// Unit Tests for the Book service
+const { sinon, chai } = require('../testModules');
+const bookService = require('../../src/services/bookService');
+
+// Add prototyp for should
+chai.should();
+
+describe('bookService', function () {
+  describe('getAllBooks', function () {
+    it('should call getAllBooks once', function () {
+
+    });
+
+    it('should return all books', function () {
+
+    });
+  });
+
+  describe('getBookById', function () {
+
+  });
+});
