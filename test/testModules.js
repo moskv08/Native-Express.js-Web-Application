887a8262924933a76016ef32ea98771c38c044e5
Fabrizio <Fabrizio_39@hotmail.com>
2019-10-01 21:23:32 +0200

    create a test modul repo

---
+++ test/testModules.js
@@ -0,0 +1,14 @@
+// Import all testing modules
+const assert = require('assert');
+const mocha = require('mocha');
+const { expect } = require('chai');
+const should = require('chai').should();
+const sinon = require('sinon');
+
+module.exports = {
+  assert,
+  mocha,
+  expect,
+  should,
+  sinon,
+};
