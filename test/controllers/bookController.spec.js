36958206e63d14e164a251b01c6529cc266b45e7
Fabrizio <Fabrizio_39@hotmail.com>
2019-10-01 21:24:06 +0200

    test bookController getAllBooks function

--- test/controllers/bookController.spec.js
+++ test/controllers/bookController.spec.js
@@ -1,7 +1,6 @@
-const assert = require('assert');
-const { expect } = require('chai');
-const should = require('chai').should();
-
+// Bring all test modules
+// eslint-disable-next-line no-unused-vars
+const { assert, expect, should, sinon } = require('../testModules');
 const bookController = require('../../src/controllers/bookController');
 
 describe('bookController', function () {
@@ -17,10 +16,23 @@ describe('bookController', function () {
       const objB = { name: 'John' };
       objA.should.deep.equal(objB);
     });
-    it('should reject a not existing user');
+    it('should redirect a non existing user to index');
   });
 
   describe('getAllBooks', function () {
+    it('should render bookListView', function (done) {
+      const req = {};
+      const res = {
+        render: sinon.spy(),
+      };
+      const { getAllBooks } = bookController({}, {});
+      getAllBooks(res, req);
+      // eslint-disable-next-line no-unused-expressions
+      res.render.calledOnce.should.be.true;
+      res.render.firstCall.args[0].should.equal('bookListView');
+      done();
+    });
+
     it('should return all available books');
     it('should return an error message in case something went wrong');
   });
